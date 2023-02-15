b98337cc142d8c0edddca4a04841ed161c933926
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-02-15 13:25:30 +0200

    add task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+
+const imageGalleryHtml = galleryItems
+  .map(
+    el =>
+      `<a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+  </a>`
+  )
+  .join('');
+
+galleryEl.innerHTML = imageGalleryHtml;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
