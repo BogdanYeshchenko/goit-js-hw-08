3afe06ec772f20161e081ef9685c331207536c6e
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-02-15 15:40:01 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const lastPlayTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(lastPlayTime);
+
+const getLiveTime = () => {
+  player.getCurrentTime().then(function (seconds) {
+    const liveTime = seconds;
+    localStorage.setItem('videoplayer-current-time', liveTime);
+    console.log(liveTime);
+  });
+};
+
+const throttleGetLiveTime = throttle(getLiveTime, 1000);
+
+player.on('timeupdate', throttleGetLiveTime);
